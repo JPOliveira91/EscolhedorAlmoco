0dca3b02eae3e842ba3127a25ebdfa2635ad41c2
JPOliveira91 <jp_oliveira1991@hotmail.com>
2019-07-10 09:19:08 -0300

    Scripts para criar e popular tabelas

---
+++ Scripts/01 - DDL - Criação de tabelas iniciais.sql
@@ -0,0 +1,47 @@
+CREATE TABLE Custo (
+	IdCusto INT NOT NULL,
+	DescricaoCusto VARCHAR(50) NULL,
+	CONSTRAINT PK_Custo PRIMARY KEY (IdCusto)
+)
+
+CREATE TABLE Restaurante(
+	IdRestaurante INT NOT NULL,
+	Nome VARCHAR(50) NULL,
+	Apelido VARCHAR(50) NULL,
+	Endereco VARCHAR(100) NULL,
+	Observacao VARCHAR(100) NULL,
+	CONSTRAINT PK_Custo PRIMARY KEY (IdCusto)
+)
+
+CREATE TABLE Tipo(
+	IdTipo INT NOT NULL,
+	DescricaoTipo VARCHAR(50) NULL,
+	CONSTRAINT PK_Tipo PRIMARY KEY (IdTipo)
+)
+
+CREATE TABLE Veto(
+	IdVeto INT NOT NULL,
+	DescricaoVeto VARCHAR(50) NULL,
+	CONSTRAINT PK_Veto PRIMARY KEY (IdVeto)
+)
+
+CREATE TABLE Restaurante_Custo(
+	IdRestaurante INT NOT NULL,
+	IdCusto INT NOT NULL,
+	CONSTRAINT FK_RestauranteCusto_Restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante (IdRestaurante),
+	CONSTRAINT FK_RestauranteCusto_Custo FOREIGN KEY (IdCusto) REFERENCES Custo (IdCusto)
+)
+
+CREATE TABLE Restaurante_Tipo(
+	IdRestaurante INT NOT NULL,
+	IdTipo INT NOT NULL,
+	CONSTRAINT FK_RestauranteTipo_Restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante (IdRestaurante),
+	CONSTRAINT FK_RestauranteTipo_Tipo FOREIGN KEY (IdTipo) REFERENCES Tipo (IdTipo)
+)
+
+CREATE TABLE Restaurante_Veto(
+	IdRestaurante INT NOT NULL,
+	IdVeto INT NOT NULL,
+	CONSTRAINT FK_RestauranteVeto_Restaurante FOREIGN KEY (IdRestaurante) REFERENCES Restaurante (IdRestaurante),
+	CONSTRAINT FK_RestauranteVeto_Veto FOREIGN KEY (IdVeto) REFERENCES Veto (IdVeto)
+)
---
+++ Scripts/01 - DML - Massa da dados inicial.sql
@@ -0,0 +1,32 @@
+-- Tabela Custo
+INSERT INTO Custo
+VALUES (1, 'Barato')
+
+INSERT INTO Custo
+VALUES (2, 'Médio')
+
+INSERT INTO Custo
+VALUES (3, 'Caro')
+
+-- Tabela Tipo
+INSERT INTO Tipo
+VALUES (1, 'Besteira')
+
+INSERT INTO Tipo
+VALUES (2, 'PF')
+
+INSERT INTO Tipo
+VALUES (3, 'Quilo')
+
+INSERT INTO Tipo
+VALUES (4, 'Buffet')
+
+-- Tabela Veto
+INSERT INTO Veto
+VALUES (1, 'André')
+
+INSERT INTO Veto
+VALUES (2, 'Juliano')
+
+INSERT INTO Veto
+VALUES (3, 'Marcelo')
